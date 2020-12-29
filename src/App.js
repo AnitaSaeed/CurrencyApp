0f07a5343bdd34aee7be34ce154a70ec8f5efc17
AnitaSaeed <Anitasaeed76@yahoo.com>
2020-12-29 19:09:09 +0330

    add style

--- src/App.js
+++ src/App.js
@@ -22,89 +22,91 @@ function App() {
         <div>
         {(typeof currencys.sana != "undefined") ? 
         (<div>
-          <div>
-            <div>
+          <div className="box-body">
+            <div className="box">
             <div>دلار آمریکا</div>
-            <div>{currencys.sana.data[0].p}</div>
-            <div>{currencys.sana.data[16].p}</div>
+            <div>خرید: {currencys.sana.data[0].p}</div>
+            <div>فروش: {currencys.sana.data[16].p}</div>
             </div>
 
-            <div>
+            <div className="box">
             <div>یورو</div>
-            <div>{currencys.sana.data[1].p}</div>
-            <div>{currencys.sana.data[17].p}</div>
+            <div>خرید:{currencys.sana.data[1].p}</div>
+            <div>فروش: {currencys.sana.data[17].p}</div>
             </div>
-            <div>
-            <div>درهم امارات</div>
-            <div>{currencys.sana.data[2].p}</div>
-            <div>{currencys.sana.data[18].p}</div>
+            <div  className="box">
+            <div >درهم امارات</div>
+            <div>خرید:{currencys.sana.data[2].p}</div>
+            <div>فروش: {currencys.sana.data[18].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>روپیه هند</div>
-            <div>{currencys.sana.data[3].p}</div>
-            <div>{currencys.sana.data[19].p}</div>
+            <div>خرید:{currencys.sana.data[3].p}</div>
+            <div>فروش: {currencys.sana.data[19].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>لیر ترکیه</div>
-            <div>{currencys.sana.data[4].p}</div>
-            <div>{currencys.sana.data[20].p}</div>
+            <div>خرید:{currencys.sana.data[4].p}</div>
+            <div>فروش: {currencys.sana.data[20].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>روبل روسیه</div>
-            <div>{currencys.sana.data[5].p}</div>
-            <div>{currencys.sana.data[21].p}</div>
+            <div>خرید:{currencys.sana.data[5].p}</div>
+            <div>فروش: {currencys.sana.data[21].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>یوان چین</div>
-            <div>{currencys.sana.data[6].p}</div>
-            <div>{currencys.sana.data[22].p}</div>
+            <div>خرید:{currencys.sana.data[6].p}</div>
+            <div>فروش: {currencys.sana.data[22].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>وون کره جنوبی</div>
-            <div>{currencys.sana.data[7].p}</div>
-            <div>{currencys.sana.data[23].p}</div>
+            <div>خرید:{currencys.sana.data[7].p}</div>
+            <div>فروش: {currencys.sana.data[23].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>فرانک سوئیس</div>
-            <div>{currencys.sana.data[8].p}</div>
-            <div>{currencys.sana.data[24].p}</div>
+            <div>خرید:{currencys.sana.data[8].p}</div>
+            <div>فروش: {currencys.sana.data[24].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>ین ژاپن</div>
-            <div>{currencys.sana.data[9].p}</div>
-            <div>{currencys.sana.data[25].p}</div>
+            <div>خرید:{currencys.sana.data[9].p}</div>
+            <div>فروش: {currencys.sana.data[25].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>دلار کانادا</div>
-            <div>{currencys.sana.data[10].p}</div>
-            <div>{currencys.sana.data[26].p}</div>
+            <div>خرید:{currencys.sana.data[10].p}</div>
+            <div>فروش: {currencys.sana.data[26].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>پوند انگلیس</div>
-            <div>{currencys.sana.data[11].p}</div>
-            <div>{currencys.sana.data[27].p}</div>
+            <div>خرید:{currencys.sana.data[11].p}</div>
+            <div>فروش: {currencys.sana.data[27].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>کرون سوئد</div>
-            <div>{currencys.sana.data[12].p}</div>
-            <div>{currencys.sana.data[27].p}</div>
+            <div>خرید:{currencys.sana.data[12].p}</div>
+            <div>فروش: {currencys.sana.data[27].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>کرون نروژ</div>
-            <div>{currencys.sana.data[13].p}</div>
-            <div>{currencys.sana.data[0].p}</div>
+            <div>خرید:{currencys.sana.data[13].p}</div>
+            <div>فروش: {currencys.sana.data[0].p}</div>
             </div>
-            <div>
+            <div className="box">
             <div>دینار عراق</div>
-            <div>{currencys.sana.data[14].p}</div>
-            <div>{currencys.sana.data[0].p}</div>
+            <div>خرید:{currencys.sana.data[14].p}</div>
+            <div>فروش:</div>
             </div>
-            <div>دلار کانادا</div>
+            <div className="box">
+            <div>دلار استرالیا</div>
             <div>{currencys.sana.data[15].p}</div>
-            <div>{currencys.sana.data[0].p}</div>
+            <div>فروش:</div>
+            </div>
             </div>
 
-          </div>
+        </div>
        
         )
         
